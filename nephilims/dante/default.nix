2568c9512f5a81e88b692b590e935c5d21b6454e
Felix <zen9.felix@gmail.com>
2023-02-18 23:53:23 +0800

    Temporary remove packages

--- nephilims/dante/default.nix
+++ nephilims/dante/default.nix
@@ -36,18 +36,18 @@
 
   environment = {
     variables = { EDITOR = "hx"; };
-    systemPackages = with pkgs;
-      let
-        clis = [ which bat curl wget exa fd fzf tree ripgrep jq fx ]
-          ++ [ git tig lazygit ]
-          ++ [ irssi httpie tokei zip tealdeer helix hexyl unrar ]
-          ++ [ dua duf glances htop btop iotop ]
-          ++ [ graphviz jq tesseract poppler_utils gdb ]
-          ++ [ tcpdump inetutils dig socat netcat ];
-        others = [ duplicity cbonsai slack obsidian zathura ];
-        browsers = [ brave firefox ];
-        os = [ lxappearance gthumb maim pavucontrol ranger ];
-      in clis ++ others ++ browsers ++ os;
+    # systemPackages = with pkgs;
+    #   let
+    #     clis = [ which bat curl wget exa fd fzf tree ripgrep jq fx ]
+    #       ++ [ git tig lazygit ]
+    #       ++ [ irssi httpie tokei zip tealdeer helix hexyl unrar ]
+    #       ++ [ dua duf glances htop btop iotop ]
+    #       ++ [ graphviz jq tesseract poppler_utils gdb ]
+    #       ++ [ tcpdump inetutils dig socat netcat ];
+    #     others = [ duplicity cbonsai slack obsidian zathura ];
+    #     browsers = [ brave firefox ];
+    #     os = [ lxappearance gthumb maim pavucontrol ranger ];
+    #   in clis ++ others ++ browsers ++ os;
   };
 
 }
