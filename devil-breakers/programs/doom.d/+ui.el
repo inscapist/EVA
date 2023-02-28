0b55f436d6022fc17f14e76a5e66d00dd4c43fe6
Felix <zen9.felix@gmail.com>
2023-02-28 20:42:30 +0800

    Fix missing font

--- devil-breakers/programs/doom.d/+ui.el
+++ devil-breakers/programs/doom.d/+ui.el
@@ -5,16 +5,20 @@
 ;; =======================================================================
 
 ;; custom variables
-(setq felix/default-font "Monoid"
+(setq felix/default-font "Monoid Nerd Font"
       felix/default-font-size 25
       felix/default-font-weight 'extra-light ;; refer font-weight-table variable
       felix/muted-font-height 0.95
-      felix/modeline-font "Monoid"
+      felix/modeline-font "Monoid Nerd Font"
       felix/modeline-height 0.8)
 
 ;; font settings
-(setq doom-font (font-spec :family felix/default-font
-                           :size felix/default-font-size))
+(setq doom-themes-enable-bold t
+      doom-themes-enable-italic t
+      doom-font (font-spec :family felix/default-font
+                           :size felix/default-font-size
+                           :weight felix/default-font-weight)
+      all-the-icons-scale-factor 0.7)
 
 
 ;; use kaolin theme
@@ -29,15 +33,15 @@
 
 
 ;; customize faces. list with SPC-h-F
-;; (custom-set-faces!
-;;   `(font-lock-comment-face :slant italic :weight thin :height ,felix/muted-font-height)
-;;   `(font-lock-keyword-face :slant italic :weight thin :height ,felix/muted-font-height)
-;;   `(font-lock-function-name-face :slant normal :weight normal)
-;;   `(mode-line :height ,felix/modeline-height :family ,felix/modeline-font)
-;;   `(mode-line-inactive :height ,felix/modeline-height ,felix/modeline-font))
+(custom-set-faces!
+  `(font-lock-comment-face :slant italic :weight thin :height ,felix/muted-font-height)
+  `(font-lock-keyword-face :slant italic :weight thin :height ,felix/muted-font-height)
+  `(font-lock-function-name-face :slant normal :weight normal)
+  `(mode-line :height ,felix/modeline-height :family ,felix/modeline-font)
+  `(mode-line-inactive :height ,felix/modeline-height ,felix/modeline-font))
 
 ;; performance hack
-;;(setq inhibit-compacting-font-caches t)
+(setq inhibit-compacting-font-caches t)
 
 ;; thinner modeline for (modeline +light)
 (setq +modeline-height 18)
