239f639aff9d3c87e5591db9a04c7044eb0a78b4
Felix <zen9.felix@gmail.com>
2023-03-01 09:24:26 +0800

    Try emacsGit

--- devil-breakers/programs/emacs.nix
+++ devil-breakers/programs/emacs.nix
@@ -10,7 +10,8 @@
   programs.emacs = {
     enable = true;
     # package = emacs-overlay.packages.${system}.emacsPgtk;
-    package = pkgs.emacsPgtk;
+    # package = pkgs.emacsPgtk;
+    package = pkgs.emacsGit;
   };
 
   home.file.".doom.d" = {
