f3d1d8c1a28a6828f4eecc2e3990c4769b1b1d3a
felix <zen9.felix@gmail.com>
2023-03-07 16:33:19 +0800

    Add 1pass secret

--- sins/secrets.nix
+++ sins/secrets.nix
@@ -1,7 +1,9 @@
 let
+  # users
   sparda =
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKtsjUN63tlgndK6fx+hHPVo7rhncnIb+Y6A5ftx3vSY";
 
+  # systems
   vergil =
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILbQuvzO7tRcdlwt462EGj/iqDKm9Clq2bH4A4B+os5x";
 
@@ -9,4 +11,5 @@ let
 in {
   "zsh_private.age".publicKeys = keys;
   "wg0_conf.age".publicKeys = keys;
+  "1pass.age".publicKeys = keys;
 }
