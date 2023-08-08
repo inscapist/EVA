2ab2ef7219532f0f71d271608ceb2a9684f3f573
Xilef <zen9.felix@gmail.com>
2023-08-08 21:51:43 +0800

    Remove mock timers

--- nephilims/vergil/default.nix
+++ nephilims/vergil/default.nix
@@ -42,24 +42,4 @@
     getty.autologinUser = user;
     openssh = { enable = true; };
   };
-
-  systemd.timers."hello-world" = {
-    wantedBy = [ "timers.target" ];
-    timerConfig = {
-      OnBootSec = "5m";
-      OnUnitActiveSec = "5m";
-      Unit = "hello-world.service";
-    };
-  };
-
-  systemd.services."hello-world" = {
-    script = ''
-      set -eu
-      ${pkgs.coreutils}/bin/echo "Hello World"
-    '';
-    serviceConfig = {
-      Type = "oneshot";
-      User = "root";
-    };
-  };
 }
