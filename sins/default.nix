6d5661d04eb976c1c8062022e3ab0b0c7f972f75
Xilef <zen9.felix@gmail.com>
2023-08-08 22:23:42 +0800

    Make restic-env reusable outside systemd

--- sins/default.nix
+++ sins/default.nix
@@ -12,7 +12,10 @@ with inputs; {
   };
   age.secrets.wg0_conf = { file = ./wg0_conf.age; };
   age.secrets.userpass = { file = ./userpass.age; };
-  age.secrets.restic_env = { file = ./restic_env.age; };
+  age.secrets.restic_env = {
+    file = ./restic_env.age;
+    path = "/etc/restic-env";
+  };
   age.secrets.restic_password = {
     file = ./restic_password.age;
     path = "/etc/restic-password";
