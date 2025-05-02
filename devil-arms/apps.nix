604683b0fcf649f64ba623f1b7d1cec903c13f49
felix <zen9.felix@gmail.com>
2025-05-02 18:54:31 +0800

    Disable apps

--- devil-arms/apps.nix
+++ devil-arms/apps.nix
@@ -2,14 +2,14 @@
 
 {
   programs.steam = {
-    enable = true;
+    enable = false;
     # remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
     # dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
   };
 
   services = {
     ollama = {
-      enable = true;
+      enable = false;
       acceleration = "cuda";
       package = pkgs.ollama.override { acceleration = "cuda"; };
 
@@ -17,7 +17,7 @@
       loadModels = [ "deepseek-r1:8b" ];
     };
     open-webui = {
-      enable = true;
+      enable = false;
       port = 11111;
       environment = {
         OLLAMA_API_BASE_URL = "http://127.0.0.1:11434";
