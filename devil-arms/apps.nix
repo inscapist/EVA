c95dbb0e53c38c4ff285d2147440842092c95aa0
felix <zen9.felix@gmail.com>
2025-05-09 22:14:59 +0800

    Save space

--- devil-arms/apps.nix
+++ devil-arms/apps.nix
@@ -9,7 +9,7 @@
 
   environment.systemPackages = with inputs.browser-previews.packages.${pkgs.system}; [
     # google-chrome # Stable Release
-    google-chrome-beta # Beta Release
+    # google-chrome-beta # Beta Release
     # google-chrome-dev # Dev Release
   ];
 
