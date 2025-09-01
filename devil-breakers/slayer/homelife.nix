cf8e1868a870421d75e910b685f502dd2ec0b58f
felix <zen9.felix@gmail.com>
2025-09-01 15:15:55 +0800

    Add reset-desktops helper script

--- devil-breakers/slayer/homelife.nix
+++ devil-breakers/slayer/homelife.nix
@@ -19,7 +19,22 @@
     # nyancat
 
     spotify
-    # discord
     jamesdsp
+
+    (pkgs.writeShellScriptBin "reset-desktops" ''
+      # First, switch to each workspace to ensure it exists
+      for i in {1..9}; do
+        ${pkgs.i3}/bin/i3-msg "workspace $i" >/dev/null 2>&1
+      done
+
+      # Now move workspaces to correct outputs
+      for i in {1..5}; do
+        ${pkgs.i3}/bin/i3-msg "workspace $i; move workspace to output DP-4"
+      done
+
+      for i in {6..9}; do
+        ${pkgs.i3}/bin/i3-msg "workspace $i; move workspace to output eDP-1"
+      done
+    '')
   ];
 }
