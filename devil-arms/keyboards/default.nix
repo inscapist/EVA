6c4172840c2aac83d425087e692d5423a67f1c75
felix <zen9.felix@gmail.com>
2025-12-19 22:52:05 +0800

    Cleanup code

--- devil-arms/keyboards/default.nix
+++ devil-arms/keyboards/default.nix
@@ -24,103 +24,106 @@
 
   # QMK/VIA
   hardware.keyboard.qmk.enable = true;
-  services.udev.packages = [ pkgs.via ];
+  services = {
+    udev = {
+      packages = [ pkgs.via ];
+      extraRules = ''
+        ATTRS{name}=="Keychron K7", SYMLINK+="keychron-k7-blue"
+      '';
+    };
 
-  services.libinput = {
-    enable = true;
-    touchpad.disableWhileTyping = true;
+    libinput = {
+      enable = true;
+      touchpad.disableWhileTyping = true;
+    };
+
+    # https://github.com/rvaiya/keyd/blob/master/docs/keyd.scdoc
+    # https://github.com/rvaiya/keyd/blob/2338f11b1ddd81eaddd957de720a3b4279222da0/t/keys.py#L31
+    keyd = {
+      enable = false;
+      keyboards.default = {
+        ids = [ "*" ];
+        settings = {
+          main = {
+            # SK71 remaps
+            insert = "capslock";
+            home = "mute";
+            leftmeta = "leftalt";
+            rightmeta = "rightalt";
+            end = "brightnessup";
+            delete = "brightnessdown";
+            pageup = "volumeup";
+            pagedown = "volumedown";
+
+            # remap escape
+            # escape = "grave";
+
+            # overloads
+            capslock = "overload(nav, esc)";
+            tab = "overload(control, tab)";
+
+            # introduce additional modifiers
+            space = "lettermod(alt, space, 150, 200)";
+            escape = "lettermod(win, grave, 150, 200)";
+            backslash = "lettermod(control, backslash, 150, 200)";
+          };
+          "win" = {
+            h = "C-A-h";
+            l = "C-A-l";
+          };
+          "nav" = {
+            # r = "layer(nav-r)";
+            # w = "layer(nav-w)";
+            # s = "layer(nav-s)";
+            b = "left";
+            s = "down";
+            t = "up";
+            r = "right";
+
+            h = "left";
+            j = "down";
+            k = "up";
+            l = "right";
+
+            p = "C-A-h";
+            n = "C-A-l";
+            minus = "C-M-l";
+            equal = "C-M-h";
+            leftbrace = "C-A-h";
+            rightbrace = "C-A-l";
+
+            m = "A-S-h";
+            z = "A-S-l";
+            comma = "A-S-h";
+            dot = "A-S-l";
+
+            enter = "C-enter";
+          };
+
+          # resize
+          # "nav-r" = {
+          #   h = "C-M-l";
+          #   l = "C-M-h";
+          # };
+          # # window
+          # "nav-w" = {
+          #   h = "C-A-h";
+          #   l = "C-A-l";
+          # };
+          # # shift
+          # "nav-s" = {
+          #   h = "A-S-h";
+          #   l = "A-S-l";
+          # };
+        };
+      };
+    };
   };
   environment.etc."libinput/local-overrides.quirks".text = ''
     [Serial Keyboards]
     MatchUdevType=keyboard
     MatchName=keyd*keyboard
     AttrKeyboardIntegration=internal
-  '';
-
-  # https://github.com/rvaiya/keyd/blob/master/docs/keyd.scdoc
-  # https://github.com/rvaiya/keyd/blob/2338f11b1ddd81eaddd957de720a3b4279222da0/t/keys.py#L31
-  services.keyd = {
-    enable = false;
-    keyboards.default = {
-      ids = [ "*" ];
-      settings = {
-        main = {
-          # SK71 remaps
-          insert = "capslock";
-          home = "mute";
-          leftmeta = "leftalt";
-          rightmeta = "rightalt";
-          end = "brightnessup";
-          delete = "brightnessdown";
-          pageup = "volumeup";
-          pagedown = "volumedown";
-
-          # remap escape
-          # escape = "grave";
-
-          # overloads
-          capslock = "overload(nav, esc)";
-          tab = "overload(control, tab)";
-
-          # introduce additional modifiers
-          space = "lettermod(alt, space, 150, 200)";
-          escape = "lettermod(win, grave, 150, 200)";
-          backslash = "lettermod(control, backslash, 150, 200)";
-        };
-        "win" = {
-          h = "C-A-h";
-          l = "C-A-l";
-        };
-        "nav" = {
-          # r = "layer(nav-r)";
-          # w = "layer(nav-w)";
-          # s = "layer(nav-s)";
-          b = "left";
-          s = "down";
-          t = "up";
-          r = "right";
-
-          h = "left";
-          j = "down";
-          k = "up";
-          l = "right";
-
-          p = "C-A-h";
-          n = "C-A-l";
-          minus = "C-M-l";
-          equal = "C-M-h";
-          leftbrace = "C-A-h";
-          rightbrace = "C-A-l";
-
-          m = "A-S-h";
-          z = "A-S-l";
-          comma = "A-S-h";
-          dot = "A-S-l";
-
-          enter = "C-enter";
-        };
-
-        # resize
-        # "nav-r" = {
-        #   h = "C-M-l";
-        #   l = "C-M-h";
-        # };
-        # # window
-        # "nav-w" = {
-        #   h = "C-A-h";
-        #   l = "C-A-l";
-        # };
-        # # shift
-        # "nav-s" = {
-        #   h = "A-S-h";
-        #   l = "A-S-l";
-        # };
-      };
-    };
-  };
-
-  services.udev.extraRules = ''
-    ATTRS{name}=="Keychron K7", SYMLINK+="keychron-k7-blue"
   '';
 
   # services.kmonad = {
--- devil-arms/networking.nix
+++ devil-arms/networking.nix
@@ -5,8 +5,33 @@
   ...
 }:
 {
-  services.tailscale.enable = false;
-  services.openssh.enable = true;
+  services = {
+    tailscale.enable = false;
+    openssh.enable = true;
+    resolved = {
+      enable = true;
+      dnssec = "allow-downgrade";
+      dnsovertls = "opportunistic";
+      llmnr = "true";
+      fallbackDns = [
+        "45.90.28.0#2977d3.dns.nextdns.io"
+        "2a07:a8c0::#2977d3.dns.nextdns.io"
+        "45.90.30.0#2977d3.dns.nextdns.io"
+        "2a07:a8c1::#2977d3.dns.nextdns.io"
+      ];
+      extraConfig = ''
+        Domains=~.
+        MulticastDNS=true
+        Cache=yes
+        DNSStubListener=yes
+      '';
+    };
+
+    # https://community.cloudflare.com/t/how-to-register-into-a-team-with-linux-and-warp-cli/627971
+    cloudflare-warp = {
+      enable = false;
+    };
+  };
 
   networking = {
     useDHCP = false;
@@ -63,30 +88,6 @@
         "9.9.9.9"
       ];
     };
-  };
-
-  services.resolved = {
-    enable = true;
-    dnssec = "allow-downgrade";
-    dnsovertls = "opportunistic";
-    llmnr = "true";
-    fallbackDns = [
-      "45.90.28.0#2977d3.dns.nextdns.io"
-      "2a07:a8c0::#2977d3.dns.nextdns.io"
-      "45.90.30.0#2977d3.dns.nextdns.io"
-      "2a07:a8c1::#2977d3.dns.nextdns.io"
-    ];
-    extraConfig = ''
-      Domains=~.
-      MulticastDNS=true
-      Cache=yes
-      DNSStubListener=yes
-    '';
-  };
-
-  # https://community.cloudflare.com/t/how-to-register-into-a-team-with-linux-and-warp-cli/627971
-  services.cloudflare-warp = {
-    enable = false;
   };
 
   # networking.networkmanager.unmanaged = [ "CloudflareWARP" ];
