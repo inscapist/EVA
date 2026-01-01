f290a22cff4982aee2155b638bd67f56c5cbbd39
felix <zen9.felix@gmail.com>
2026-01-01 16:51:07 +0800

    Aliases and nix substituters

--- devil-arms/os.nix
+++ devil-arms/os.nix
@@ -30,12 +30,14 @@
         "https://nix-config.cachix.org"
         "https://nix-community.cachix.org"
         "https://hyprland.cachix.org"
+        "https://nixpkgs-ruby.cachix.org"
       ];
       trusted-public-keys = [
         "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
         "nix-config.cachix.org-1:Vd6raEuldeIZpttVQfrUbLvXJHzzzkS0pezXCVVjDG4="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+        "nixpkgs-ruby.cachix.org-1:vrcdi50fTolOxWCZZkw0jakOnUI1T19oYJ+PRYdK4SM="
       ];
       experimental-features = [
         "nix-command"
