caa8054ed3ea65d16f1c259a3cc4dd35113caeff
felix <zen9.felix@gmail.com>
2026-01-08 10:53:33 +0800

    Revert "Remove old dns"
    This reverts commit ae265cdd74d1ad2f14f3bfc0f2c8d48622f24fcd.

--- devil-arms/networking.nix
+++ devil-arms/networking.nix
@@ -14,6 +14,12 @@
       dnssec = "allow-downgrade";
       dnsovertls = "opportunistic";
       llmnr = "true";
+      fallbackDns = [
+        "45.90.28.0#2977d3.dns.nextdns.io"
+        "2a07:a8c0::#2977d3.dns.nextdns.io"
+        "45.90.30.0#2977d3.dns.nextdns.io"
+        "2a07:a8c1::#2977d3.dns.nextdns.io"
+      ];
       extraConfig = ''
         Domains=~.
         MulticastDNS=true
@@ -242,6 +248,8 @@
     nameservers = [
       "1.1.1.1"
       "1.0.0.1"
+      "45.90.28.0"
+      "45.90.30.0"
     ];
 
     firewall = {
